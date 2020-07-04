b87f98e909df39f5915de9690f1c7fd646d3033e
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-07-04 18:25:56 +0200

    create assert_all_colors_allowed

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from os import path, mkdir
+from typing import Optional, Set, Tuple
+
 from PIL import Image
 
 
-def assert_all_black_white(img: Image.Image):
+def assert_all_colors_allowed(img: Image.Image, allowed_colors: Optional[Set[Tuple[int, int, int]]] = None):
+    if allowed_colors is None:
+        allowed_colors = {(255, 255, 255), (0, 0, 0)}
+
     width, height = img.size
     for y in range(height):
         for x in range(width):
-            r, g, b = img.getpixel((x, y))
-            assert r == g == b
-            assert r == 0 or r == 255
+            assert img.getpixel((x, y)) in allowed_colors
 
 
 if not path.exists('build'):
@@ -16,9 +19,9 @@ if not path.exists('build'):
 
 input_img: Image.Image = Image.open('template1_64x64.png').convert("RGB")
 assert input_img.size == (64, 64)
-assert_all_black_white(input_img)
+assert_all_colors_allowed(input_img)
 
 output_img = input_img.resize((512, 512), Image.NEAREST)
-assert_all_black_white(output_img)
+assert_all_colors_allowed(output_img)
 
 output_img.save("build/test1.png")
