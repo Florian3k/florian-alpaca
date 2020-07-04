82df3c201a3240673c8b11346c89b83b211a9683
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-07-04 18:15:44 +0200

    ignore alpha channel

--- main.py
+++ main.py
@@ -6,17 +6,15 @@ def assert_all_black_white(img: Image.Image):
     width, height = img.size
     for y in range(height):
         for x in range(width):
-            r, g, b, a = img.getpixel((x, y))
+            r, g, b = img.getpixel((x, y))
             assert r == g == b
-            assert a == 255
             assert r == 0 or r == 255
 
 
 if not path.exists('build'):
     mkdir('build')
 
-input_img: Image.Image = Image.open('template1_64x64.png')
-
+input_img: Image.Image = Image.open('template1_64x64.png').convert("RGB")
 assert input_img.size == (64, 64)
 assert_all_black_white(input_img)
 
