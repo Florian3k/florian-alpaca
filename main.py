e1b9d4bb49969191dc5088c123147036d191a1e4
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-07-05 10:04:53 +0200

    add image coloring

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from os import path, mkdir
 from typing import Optional, Set, Tuple
 
 from PIL import Image
+import numpy as np
 
 
 def assert_all_colors_allowed(img: Image.Image, allowed_colors: Optional[Set[Tuple[int, int, int]]] = None):
@@ -21,7 +22,28 @@ input_img: Image.Image = Image.open('template1_64x64.png').convert("RGB")
 assert input_img.size == (64, 64)
 assert_all_colors_allowed(input_img)
 
-output_img = input_img.resize((512, 512), Image.NEAREST)
-assert_all_colors_allowed(output_img)
+change_colors = [
+    ((255, 255, 255), (255, 255, 255)),  # background color
+    ((0, 0, 0), (53, 53, 53))  # alpaca color
+]
+
+data = np.array(input_img)
+r, g, b = data.swapaxes(0, 2).swapaxes(1, 2).copy()
+
+masks_and_colors = [
+    (
+        (r == from_r) & (g == from_g) & (b == from_b),
+        target_color
+    )
+    for (from_r, from_g, from_b), target_color
+    in change_colors
+]
+
+for mask, target_color in masks_and_colors:
+    data[mask] = target_color
+
+colored_image = Image.fromarray(data)
+output_img = colored_image.resize((512, 512), Image.NEAREST)
+assert_all_colors_allowed(output_img, {(255, 255, 255), (53, 53, 53)})
 
 output_img.save("build/test1.png")
