de2e665ebc1acebae2e603ee5217cf9f8ca7b813
Florian3k <24961583+Florian3k@users.noreply.github.com>
2021-07-29 09:54:27 +0200

    Add more flexible sizes

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from PIL import Image
 import numpy as np
 
 
+input_size = 64
+upscale_factor = 8
+output_size = input_size * upscale_factor
+
+
 def assert_all_colors_allowed(img: Image.Image, allowed_colors: Optional[Set[Tuple[int, int, int]]] = None):
     if allowed_colors is None:
         allowed_colors = {(255, 255, 255), (0, 0, 0)}
@@ -18,8 +23,8 @@ def assert_all_colors_allowed(img: Image.Image, allowed_colors: Optional[Set[Tup
 if not path.exists('build'):
     mkdir('build')
 
-input_img: Image.Image = Image.open('template1_64x64.png').convert("RGB")
-assert input_img.size == (64, 64)
+input_img: Image.Image = Image.open(f'template1_{input_size}x{input_size}.png').convert("RGB")
+assert input_img.size == (input_size, input_size)
 assert_all_colors_allowed(input_img)
 
 alpaca_color = 83, 83, 83
@@ -45,6 +50,6 @@ for mask, target_color in masks_and_colors:
     data[mask] = target_color
 
 colored_image = Image.fromarray(data)
-output_img = colored_image.resize((512, 512), Image.NEAREST)
+output_img = colored_image.resize((output_size, output_size), Image.NEAREST)
 
-output_img.save("build/florian-alpaca.png")
+output_img.save(f"build/florian-alpaca-{output_size}.png")
